ce4f3b14b6a3a033dfdb6a67388a7445b990e528
Bruno Rodrigues <bruno@agenciaeplus.com.br>
2024-04-10 10:29:45 -0300

    fix social icons

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -1,84 +1,84 @@
-import { asset } from "$fresh/runtime.ts";
-import type { JSX } from "preact";
+import { asset } from '$fresh/runtime.ts';
+import type { JSX } from 'preact';
 
-export type AvailableIcons =
-  | "Alert"
-  | "AlertError"
-  | "AlertInfo"
-  | "AlertSuccess"
-  | "AlertWarning"
-  | "ArrowsPointingOut"
-  | "Bars3"
-  | "ChevronDown"
-  | "ChevronLeft"
-  | "ChevronRight"
-  | "ChevronUp"
-  | "Close"
-  | "CreditCards"
-  | "Deco"
-  | "Diners"
-  | "Discord"
-  | "Discount"
-  | "Elo"
-  | "Elos"
-  | "Facebook"
-  | "FilterList"
-  | "Hamburger"
-  | "Heart"
-  | "House"
-  | "Instagram"
-  | "Linkedin"
-  | "Lock"
-  | "MagnifyingGlass"
-  | "MapPin"
-  | "Mastercard"
-  | "Mastercards"
-  | "Message"
-  | "Minus"
-  | "Newsletter"
-  | "OpenBook"
-  | "Phone"
-  | "Pix"
-  | "Pixs"
-  | "Plus"
-  | "QuestionMarkCircle"
-  | "Return"
-  | "Ruler"
-  | "ShoppingBag"
-  | "ShoppingCart"
-  | "Star"
-  | "Tiktok"
-  | "Trash"
-  | "Truck"
-  | "Twitter"
-  | "User"
-  | "Visa"
-  | "Visas"
-  | "WhatsApp"
-  | "XMark"
-  | "Youtube"
-  | "Zoom"
-  | "share";
+export type AvailableIcons = 
+    | 'Alert'
+    | 'AlertError'
+    | 'AlertInfo'
+    | 'AlertSuccess'
+    | 'AlertWarning'
+    | 'ArrowsPointingOut'
+    | 'Bars3'
+    | 'ChevronDown'
+    | 'ChevronLeft'
+    | 'ChevronRight'
+    | 'ChevronUp'
+    | 'Close'
+    | 'CreditCards'
+    | 'Deco'
+    | 'Diners'
+    | 'Discord'
+    | 'Discount'
+    | 'Elo'
+    | 'Elos'
+    | 'Facebook'
+    | 'FilterList'
+    | 'Hamburger'
+    | 'Heart'
+    | 'House'
+    | 'Instagram'
+    | 'Linkedin'
+    | 'Lock'
+    | 'MagnifyingGlass'
+    | 'MapPin'
+    | 'Mastercard'
+    | 'Mastercards'
+    | 'Message'
+    | 'Minus'
+    | 'Newsletter'
+    | 'OpenBook'
+    | 'Phone'
+    | 'Pix'
+    | 'Pixs'
+    | 'Plus'
+    | 'QuestionMarkCircle'
+    | 'Return'
+    | 'Ruler'
+    | 'ShoppingBag'
+    | 'ShoppingCart'
+    | 'Star'
+    | 'Tiktok'
+    | 'Trash'
+    | 'Truck'
+    | 'Twitter'
+    | 'User'
+    | 'Visa'
+    | 'Visas'
+    | 'WhatsApp'
+    | 'XMark'
+    | 'Youtube'
+    | 'Zoom'
+    | 'share';
 
 interface Props extends JSX.SVGAttributes<SVGSVGElement> {
-  id: AvailableIcons;
-  size?: number;
-}
+	id: AvailableIcons
+	size?: number
+};
 
 function Icon(
-  { id, strokeWidth = 16, size, width, height, ...otherProps }: Props,
+	{ id, strokeWidth = 16, size, width, height, ...otherProps }: Props,
 ) {
-  return (
-    <svg
-      {...otherProps}
-      width={width ?? size}
-      height={height ?? size}
-      strokeWidth={strokeWidth}
-    >
-      <use href={asset(`/sprites.svg#${id}`)} />
+	return (
+		<svg
+			{...otherProps}
+			width={width ?? size}
+			height={height ?? size}
+			strokeWidth={strokeWidth}
+		>
+			<use href={asset(`/sprites.svg#${id}`)} />
       {/* <title>{id}</title> */}
-    </svg>
-  );
+		</svg>
+	);
 }
 
 export default Icon;
